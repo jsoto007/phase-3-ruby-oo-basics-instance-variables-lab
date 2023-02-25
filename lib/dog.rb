fb39a55924f7d656d580b852abe0c46bf0bce091
Jose Soto <jsoto7087@gmail.com>
2023-02-24 23:00:17 -0500

    commit tree d7e04f3cc3898b3826e665b5785852e3df236e5c

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,42 @@
 class Dog
 
-end
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end 
+
+    def name 
+        @this_dogs_name
+    end 
+
+end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Dog
+#     def name=(dog_name)
+#         @this_dogs_name = dog_name
+#     end 
+
+#     def name 
+#         @this_dogs_name
+#     end 
+
+# end
+
+
+lassie = Dog.new
+lassie.name = "Lassie"
+puts lassie.name
+
+#We need instance variables to be able to access then in the instance of the class. To define an instance variable we use the prefix @
